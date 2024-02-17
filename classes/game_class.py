bd8c9dcb0e8997ac8cc146ce92301465bce875cb
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2024-02-17 08:26:04 +0300

    Работа с обработчиками событий

--- classes/game_class.py
+++ classes/game_class.py
@@ -1,28 +1,67 @@
 from transitions import Machine
+from functools import wraps
+
 
 class Matter(object):
     pass
 
 lump = Matter()
 
-states=['not_in_game', 'in_game']
+states=['default', 'not_in_game', 'in_game']
 
 transitions = [
+    { 'trigger': 'start', 'source': 'default', 'dest': 'in_game' },
     { 'trigger': 'start', 'source': 'not_in_game', 'dest': 'in_game' },
     { 'trigger': 'end', 'source': 'in_game', 'dest': 'not_in_game' }
 ]
 
-machine = Machine(lump, states=states, transitions=transitions, initial='not_in_game')
+machine = Machine(lump, states=states, transitions=transitions, initial='default')
+
+class Handler:
+    def __init__(self) -> None:
+        self.fsm: Matter = None
+        self.command: str = ""
+        self.handlers = []
+
+
+    def filter_update(self, states: list[str] = [], commands: list[str] = []):
+        def real_decorator(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                if (self.fsm.state in states or not states) and (self.command  in commands or not commands):
+                    return func(*args, **kwargs)
+                return None
+            return wrapper
+
+        return real_decorator
+        
+
+
+
 
 class Game:
     def __init__(self) -> None:
         self.mistakes = 0
         self.word = ""
+        self.guessed_word = ""
         self.used_letters = []
 
     def clear(self):
         self.mistakes = 0
         self.word = ""
+        self.guessed_word = ""
+        self.used_letters = []
+
+    @staticmethod
+    def open_letters(hidden: str, word: str, letter: str) -> str:
+        new_hidden: str = ""
+        for i in range(len(word)):
+            if word[i] == letter:
+                new_hidden += word[i]
+            else:
+                new_hidden += hidden[i]
+        return new_hidden
+
 
 if __name__ == "__main__":
     print(lump.state)
--- main.py
+++ main.py
@@ -1,21 +1,54 @@
-#from utils.funcs import Filter
-
 import classes
-from classes.game_class import lump
+from classes.game_class import lump, Handler
+from random import choice
 
 
-command: str = ""
+MAX_MISTAKES = 5
 
-#@Filter(lump, "not_in_game", command)
-def start_game():
-    print("dkwodwdk")
-    game.clear()
+handler = Handler()
+handler.fsm = lump
+
+
+@handler.filter_update(states=["default", "not_in_game"], commands=["start"])
+def start_game(game: classes.Game):
+    print("Игра началась")
+    with open ("utils\\ru_dict.txt", mode = "r", encoding = "utf-8") as f:      
+        game.word = choice(f.readlines())
+    game.guessed_word = "_" * len(game.word)
+    print(f"Ваше слово: {game.guessed_word}")
     lump.start()
 
 
-func_dict = {
-    "start": start_game
-}
+@handler.filter_update(states=["in_game"], commands=["end"])
+def end_game(game: classes.Game):
+    print("Конец игры")
+    game.clear()
+    lump.end()
+
+
+@handler.filter_update(states=["in_game"])
+def guess_letter(game: classes.Game, letter: str):
+    if letter in game.used_letters:
+        print("Вы уже использовали эту букву")
+    elif letter in game.word:
+        game.used_letters.append(letter)
+        game.guessed_word = game.open_letters(game.guessed_word, game.word, letter)
+        if game.guessed_word == game.word:
+            print("Победа")
+            lump.end()
+            game.clear()
+            print("Хотите сыграть еще?")
+    else:
+        game.used_letters.append(letter)
+        game.mistakes += 1
+        if game.mistakes >= MAX_MISTAKES:
+            print("Вы проиграли")
+            lump.end()
+            game.clear()
+            print("Хотите сыграть еще?")
+        else:
+            print("Нет такой буквы")
+    print(f"Ваше слово: {game.guessed_word}, использованные буквы: {game.used_letters}")
 
 
 if __name__ == "__main__":
@@ -24,10 +57,12 @@ if __name__ == "__main__":
 
     while True:
 
-        command = input()
+        update = input()
 
-        func = func_dict.get(command, None)
-        if func:
-            func()
-        else:
-            print("Не знаю такой команды")
+        handler.command = update
+
+        start_game(game)
+
+        end_game(game)
+
+        guess_letter(game, update)
