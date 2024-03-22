544923d77522c101229bde9dc09b58d637cc4b69
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2024-03-22 11:34:20 +0300

    Фразы перенесены в lexicon

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import classes
 from classes.game_class import lump, Handler
 from random import choice
 from utils.man_pictures import man
+from lexicon.lexicon_ru import LEXICON_RU
 
 
 MAX_MISTAKES = 6
@@ -12,18 +13,18 @@ handler.fsm = lump
 
 @handler.filter_update(states=["default", "not_in_game"], commands=["start"])
 def start_game(game: classes.Game):
-    print("Игра началась")
+    print(LEXICON_RU["start_game"])
     with open ("utils\\ru_dict.txt", mode = "r", encoding = "utf-8") as f:      
         game.word = choice(f.readlines()).replace("\n", "")
     game.guessed_word = "_" * len(game.word)
-    print(f"Ваше слово: {game.guessed_word}")
+    print(f"{LEXICON_RU['word']}: {game.guessed_word}")
     lump.start()
     return True
 
 
 @handler.filter_update(states=["in_game"], commands=["end"])
 def end_game(game: classes.Game):
-    print("Конец игры")
+    print(LEXICON_RU["end_game"])
     game.clear()
     lump.end()
     return True
@@ -32,30 +33,33 @@ def end_game(game: classes.Game):
 @handler.filter_update(states=["in_game"])
 def guess_letter(game: classes.Game):
     letter: str = handler.update
+    if not (letter.isalpha() and len(letter) == 1):
+        print(LEXICON_RU["correct_letter"])
+        return True
     if letter in game.used_letters:
-        print("Вы уже использовали эту букву")
+        print(LEXICON_RU["used_letter"])
     elif letter in game.word:
         game.used_letters.append(letter)
         game.guessed_word = game.open_letters(game.guessed_word, game.word, letter)
         if game.guessed_word == game.word:
-            print("Победа")
+            print(LEXICON_RU["win"])
             lump.end()
             game.clear()
-            print("Хотите сыграть еще?")
+            print(LEXICON_RU["wanna_play"])
     else:
         game.used_letters.append(letter)
         print(man[game.mistakes])
         game.mistakes += 1
         if game.mistakes >= MAX_MISTAKES:
-            print("Вы проиграли")
-            print(f"ваше слово было: {game.word}")
+            print(LEXICON_RU["defeat"])
+            print(f"{LEXICON_RU['word_was']}: {game.word}")
             lump.end()
             game.clear()
-            print("Хотите сыграть еще?")
+            print(LEXICON_RU["wanna_play"])
             return True
         else:
-            print("Нет такой буквы")
-    print(f"Ваше слово: {game.guessed_word}, использованные буквы: {game.used_letters}")
+            print(LEXICON_RU["no_letter"])
+    print(f"{LEXICON_RU['word']}: {game.guessed_word}, {LEXICON_RU['used_letters']}: {game.used_letters}")
     return True
 
 
